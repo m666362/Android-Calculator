ec36ba1f882ac78b666c26556d69c0ed4d9cbdbd
github_username <mdrayhan9464@gmail.com>
2020-11-12 12:50:35 +0600

    raw design

---
+++ app/src/main/java/org/richit/calculator/MainActivity.java
@@ -0,0 +1,28 @@
+package org.richit.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        Button plus = findViewById( R.id.addition );
+
+
+        plus.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( MainActivity.this, "Plus", Toast.LENGTH_SHORT ).show();
+            }
+        } );
+    }
+
+}
